3d0942991789790da1d0ff35206e9f4454b85560
Beat Küng <beat-kueng@gmx.net>
2016-07-09 21:54:50 +0200

    info.py: drop '-v', always use verbose output
    There is almost no difference in run-time anymore, so we can always show
    the verbose output.

--- info.py
+++ info.py
@@ -14,22 +14,17 @@ Display information from an ULog file
 parser = argparse.ArgumentParser(description='Display information from an ULog file')
 parser.add_argument('filename', metavar='file.ulg', help='ULog input file')
 
-parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
-                   help='More verbose output (and slower)', default=False)
-
 
 args = parser.parse_args()
 ulog_file_name = args.filename
+ulog = ULog(ulog_file_name)
 
-verbose = args.verbose
-
-if verbose:
-    msg_filter = None
-else:
-    msg_filter = [] # we don't need the messages
-
-ulog = ULog(ulog_file_name, msg_filter)
-
+m1, s1 = divmod(int(ulog.start_timestamp/1e6), 60)
+h1, m1 = divmod(m1, 60)
+m2, s2 = divmod(int((ulog.last_timestamp - ulog.start_timestamp)/1e6), 60)
+h2, m2 = divmod(m2, 60)
+print("Logging start time: {:d}:{:02d}:{:02d}, duration: {:d}:{:02d}:{:02d}".format(
+    h1, m1, s1, h2, m2, s2))
 
 dropout_durations = [ dropout.duration for dropout in ulog.dropouts]
 if len(dropout_durations) == 0:
@@ -40,25 +35,18 @@ else:
                 max(dropout_durations),
                 int(sum(dropout_durations)/len(dropout_durations))))
 
+
 print("Info Messages:")
 for k in ulog.msg_info_dict:
     print(" {0}: {1}".format(k, ulog.msg_info_dict[k]))
 
-if verbose:
-    m1, s1 = divmod(int(ulog.start_timestamp/1e6), 60)
-    h1, m1 = divmod(m1, 60)
-    m2, s2 = divmod(int((ulog.last_timestamp - ulog.start_timestamp)/1e6), 60)
-    h2, m2 = divmod(m2, 60)
-    print("Logging start time: {:d}:{:02d}:{:02d}, duration: {:d}:{:02d}:{:02d}".format(
-        h1, m1, s1, h2, m2, s2))
 
-    print("")
-    print("{:<41} {:7}, {:10}".format("Name (multi id, message size in bytes)",
-        "number of data points", "total bytes"))
-    for d in ulog.data_list:
-        message_size = sum([ ULog.UNPACK_TYPES[f.type_str][1] for f in d.field_data ])
-        num_data_points = len(d.data['timestamp'])
-        name_id = "{:} ({:}, {:})".format(d.name, d.multi_id, message_size)
-        print(" {:<40} {:7d} {:10d}".format(name_id, num_data_points, message_size * num_data_points))
-    
+print("")
+print("{:<41} {:7}, {:10}".format("Name (multi id, message size in bytes)",
+    "number of data points", "total bytes"))
+for d in ulog.data_list:
+    message_size = sum([ ULog.UNPACK_TYPES[f.type_str][1] for f in d.field_data ])
+    num_data_points = len(d.data['timestamp'])
+    name_id = "{:} ({:}, {:})".format(d.name, d.multi_id, message_size)
+    print(" {:<40} {:7d} {:10d}".format(name_id, num_data_points, message_size * num_data_points))
 
