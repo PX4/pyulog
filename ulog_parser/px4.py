30e546cf9ed49e361f58a431127fa7707825d058
Beat Küng <beat-kueng@gmx.net>
2016-10-20 10:32:07 +0200

    PX4ULog: add add_roll_pitch_yaw() convenience method

--- ulog_parser/px4.py
+++ ulog_parser/px4.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 
 __author__ = "Beat Kueng"
 
+import numpy as np
+
 
 class PX4ULog:
     """
@@ -29,3 +31,29 @@ class PX4ULog:
 
         mav_type = self.ulog.initial_parameters.get('SYS_MC_EST_GROUP', None)
         return {0: 'INAV', 1: 'LPE', 2: 'EKF2'}.get(mav_type, 'unknown')
+
+
+    def add_roll_pitch_yaw(self):
+        """ convenience method to add the fields 'roll', 'pitch', 'yaw' to the
+        loaded data using the quaternion fields (does not update field_data).
+
+        Messages are: 'vehicle_attitude.q' and 'vehicle_attitude_setpoint.q_d' """
+
+        self._add_roll_pitch_yaw_to_message('vehicle_attitude')
+        self._add_roll_pitch_yaw_to_message('vehicle_attitude_setpoint', '_d')
+
+
+    def _add_roll_pitch_yaw_to_message(self, message_name, field_name_suffix = ''):
+
+        message_data_all = [ elem for elem in self.ulog.data_list if elem.name == message_name]
+        for message_data in message_data_all:
+            q=[message_data.data['q'+field_name_suffix+'['+str(i)+']'] for i in range(4)]
+            roll=np.arctan2(2.0 * (q[0] * q[1] + q[2] * q[3]),
+                    1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2]))
+            pitch = np.arcsin(2.0 * (q[0] * q[2] - q[3] * q[1]))
+            yaw = np.arctan2(2.0 * (q[0] * q[3] + q[1] * q[2]),
+                    1.0 - 2.0 * (q[2] * q[2] + q[3] * q[3]))
+            message_data.data['roll'+field_name_suffix] = roll
+            message_data.data['pitch'+field_name_suffix] = pitch
+            message_data.data['yaw'+field_name_suffix] = yaw
+
