820565a897f992cd3701249dc20193437d68286b
Mark Whitehorn <kd0aij@gmail.com>
2016-12-02 14:17:16 -0700

    add --octave output format option for parameters

--- pyulog/version.py
+++ pyulog/version.py
@@ -3,7 +3,7 @@
 short_version = '0.0.1'
 version = '0.0.1'
 full_version = '0.0.1'
-git_revision = 'bead8ac9141bc4fd32cc43f8895c4b74b5f5db63'
+git_revision = '6e74ac3a454f96a30fdb1c147b2a0d92fe380d4d'
 release = True
 
 if not release:
