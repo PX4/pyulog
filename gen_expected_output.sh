5912a2c8e1d0434cfeacf933c7b69c54217222e4
Beat Küng <beat-kueng@gmx.net>
2017-02-13 22:25:31 +0100

    tests: compare info, messages & params output against stored output

---
+++ gen_expected_output.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# generate the expected output for the unittests
+
+for f in test/*.ulg; do
+	echo "Processing $f"
+	ulog_info "$f" > "${f%.*}"_info.txt
+	ulog_messages "$f" > "${f%.*}"_messages.txt
+	ulog_params "$f" > "${f%.*}"_params.txt
+done
--- test/test_ulog2csv.py
+++ test/test_ulog2csv.py
@@ -4,11 +4,35 @@ import unittest
 from pyulog import ulog2csv, info, params, messages, extract_gps_dump
 import sys
 import tempfile
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
 
 TEST_PATH = os.path.dirname(os.path.abspath(
     inspect.getfile(inspect.currentframe())))
 
 class Test(unittest.TestCase):
+
+    def run_against_file(self, expected_output_file, test):
+        """
+        run a test and compare the output against an expected file
+        """
+        saved_stdout = sys.stdout
+        with open(expected_output_file, 'r') as file_handle:
+            expected_output = file_handle.read().strip()
+            try:
+                out = StringIO()
+                sys.stdout = out
+                test()
+                output = out.getvalue().strip()
+                assert output == expected_output
+            finally:
+                sys.stdout = saved_stdout
+                print("Got output:")
+                print(output)
+                print("\nExpected output:")
+                print(expected_output)
 
     def test_ulog2csv(self):
         tmpdir = tempfile.gettempdir()
@@ -24,7 +48,8 @@ class Test(unittest.TestCase):
             '',
             os.path.join(TEST_PATH, 'sample.ulg')
         ]
-        info.main()
+        self.run_against_file(
+                os.path.join(TEST_PATH, 'sample_info.txt'), info.main)
 
     @unittest.skip("no gps data in log file")
     def test_extract_gps_dump_cli(self):
@@ -39,14 +64,16 @@ class Test(unittest.TestCase):
             '',
             os.path.join(TEST_PATH, 'sample.ulg')
         ]
-        messages.main()
+        self.run_against_file(
+                os.path.join(TEST_PATH, 'sample_messages.txt'), messages.main)
 
     def test_params_cli(self):
         sys.argv = [
             '',
             os.path.join(TEST_PATH, 'sample.ulg')
         ]
-        params.main()
+        self.run_against_file(
+                os.path.join(TEST_PATH, 'sample_params.txt'), params.main)
 
 
 # vim: set et fenc=utf-8 ft=python ff=unix sts=4 sw=4 ts=4 : 
