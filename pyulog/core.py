1f9237f94d415539cdb72d2e5a362720c425cb61
devbharat <bharat.tak8@gmail.com>
2019-06-04 13:40:24 +0200

    Make tag 2 bytes long

--- pyulog/core.py
+++ pyulog/core.py
@@ -332,9 +332,9 @@ class ULog(object):
 
         def __init__(self, data, header):
             self.log_level, = struct.unpack('<B', data[0:1])
-            self.tag = struct.unpack('<B', data[1:2])
-            self.timestamp, = struct.unpack('<Q', data[2:10])
-            self.message = _parse_string(data[10:])
+            self.tag = struct.unpack('<H', data[1:3])
+            self.timestamp, = struct.unpack('<Q', data[3:11])
+            self.message = _parse_string(data[11:])
 
         def log_level_str(self):
             return {ord('0'): 'EMERGENCY',
