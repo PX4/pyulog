a49b48aaefffd76c450e12409083dad915966564
Bharat Tak <bharat.tak@wingtra.com>
2019-06-07 08:11:00 +0200

    Feature/add tagged logging message (#43)
    Add logging tagged msg

--- pyulog/core.py
+++ pyulog/core.py
@@ -41,6 +41,7 @@ class ULog(object):
     MSG_TYPE_SYNC = ord('S')
     MSG_TYPE_DROPOUT = ord('O')
     MSG_TYPE_LOGGING = ord('L')
+    MSG_TYPE_LOGGING_TAGGED = ord('C')
     MSG_TYPE_FLAG_BITS = ord('B')
 
     _UNPACK_TYPES = {
@@ -95,6 +96,7 @@ class ULog(object):
         self._changed_parameters = []
         self._message_formats = {}
         self._logged_messages = []
+        self._logged_messages_tagged = {}
         self._dropouts = []
         self._data_list = []
 
@@ -153,6 +155,11 @@ class ULog(object):
     def logged_messages(self):
         """ list of MessageLogging objects """
         return self._logged_messages
+
+    @property
+    def logged_messages_tagged(self):
+        """ dict of MessageLoggingTagged objects """
+        return self._logged_messages_tagged
 
     @property
     def dropouts(self):
@@ -309,6 +316,25 @@ class ULog(object):
             self.log_level, = struct.unpack('<B', data[0:1])
             self.timestamp, = struct.unpack('<Q', data[1:9])
             self.message = _parse_string(data[9:])
+
+        def log_level_str(self):
+            return {ord('0'): 'EMERGENCY',
+                    ord('1'): 'ALERT',
+                    ord('2'): 'CRITICAL',
+                    ord('3'): 'ERROR',
+                    ord('4'): 'WARNING',
+                    ord('5'): 'NOTICE',
+                    ord('6'): 'INFO',
+                    ord('7'): 'DEBUG'}.get(self.log_level, 'UNKNOWN')
+
+    class MessageLoggingTagged(object):
+        """ ULog tagged log string message representation """
+
+        def __init__(self, data, header):
+            self.log_level, = struct.unpack('<B', data[0:1])
+            self.tag = struct.unpack('<H', data[1:3])
+            self.timestamp, = struct.unpack('<Q', data[3:11])
+            self.message = _parse_string(data[11:])
 
         def log_level_str(self):
             return {ord('0'): 'EMERGENCY',
@@ -482,7 +508,8 @@ class ULog(object):
                 msg_info = self._MessageInfo(data, header)
                 self._initial_parameters[msg_info.key] = msg_info.value
             elif (header.msg_type == self.MSG_TYPE_ADD_LOGGED_MSG or
-                  header.msg_type == self.MSG_TYPE_LOGGING):
+                  header.msg_type == self.MSG_TYPE_LOGGING or
+                  header.msg_type == self.MSG_TYPE_LOGGING_TAGGED):
                 self._file_handle.seek(-(3+header.msg_size), 1)
                 break # end of section
             elif header.msg_type == self.MSG_TYPE_FLAG_BITS:
@@ -568,6 +595,12 @@ class ULog(object):
                 elif header.msg_type == self.MSG_TYPE_LOGGING:
                     msg_logging = self.MessageLogging(data, header)
                     self._logged_messages.append(msg_logging)
+                elif header.msg_type == self.MSG_TYPE_LOGGING_TAGGED:
+                    msg_log_tagged = self.MessageLoggingTagged(data, header)
+                    if msg_log_tagged.tag in self._logged_messages_tagged:
+                        self._logged_messages_tagged[msg_log_tagged.tag].append(msg_log_tagged)
+                    else:
+                        self._logged_messages_tagged[msg_log_tagged.tag] = [msg_log_tagged]
                 elif header.msg_type == self.MSG_TYPE_DATA:
                     msg_data.initialize(data, header, self._subscriptions, self)
                     if msg_data.timestamp != 0 and msg_data.timestamp > self._last_timestamp:
@@ -580,9 +613,8 @@ class ULog(object):
                     if self._debug:
                         print('_read_file_data: unknown message type: %i (%s)' %
                               (header.msg_type, chr(header.msg_type)))
-                        file_position = self._file_handle.tell()
-                        print('file position: %i (0x%x) msg size: %i' % (
-                            file_position, file_position, header.msg_size))
+                        print('file position: %i msg size: %i' % (
+                            self._file_handle.tell(), header.msg_size))
 
                     if self._check_file_corruption(header):
                         # seek back to advance only by a single byte instead of
