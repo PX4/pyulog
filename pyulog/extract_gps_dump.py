ebd16a6cdf1261c2ef9d6a367c22317649b73a0e
Roman Dvořák <romandvorak@mlab.cz>
2020-11-25 07:51:04 +0100

    extract_gps_dump: add support of multiple GNSS recievers

--- pyulog/extract_gps_dump.py
+++ pyulog/extract_gps_dump.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 
 import argparse
 import os
+import sys
 
 from .core import ULog
 
@@ -29,12 +30,17 @@ def main():
     parser.add_argument('-o', '--output', dest='output', action='store',
                         help='Output directory (default is CWD)',
                         metavar='DIR', type=lambda x: is_valid_directory(parser, x))
-    parser.add_argument('-i', '--ignore', dest='ignore', action='store_true',
+    parser.add_argument('-x', '--ignore', dest='ignore', action='store_true',
                         help='Ignore string parsing exceptions', default=False)
+    parser.add_argument('-i', '--instance', dest='required_instance', action='store',
+                        help='GPS instance. Use 0 (default)'
+                        + 'for main GPS, 1 for secondary GPS reciever.',
+                        default=0)
 
     args = parser.parse_args()
     ulog_file_name = args.filename
     disable_str_exceptions = args.ignore
+    required_instance = int(args.required_instance)
 
     msg_filter = ['gps_dump']
     ulog = ULog(ulog_file_name, msg_filter, disable_str_exceptions)
@@ -49,13 +55,13 @@ def main():
     if args.output is not None:
         output_file_prefix = os.path.join(args.output, output_file_prefix)
 
-    to_dev_filename = output_file_prefix+'_to_device.dat'
-    from_dev_filename = output_file_prefix+'_from_device.dat'
+    to_dev_filename = output_file_prefix + '_' + str(required_instance) + '_to_device.dat'
+    from_dev_filename = output_file_prefix + '_' + str(required_instance) + '_from_device.dat'
 
 
     if len(data) == 0:
         print("File {0} does not contain gps_dump messages!".format(ulog_file_name))
-        exit(0)
+        sys.exit(0)
 
     gps_dump_data = data[0]
 
@@ -63,7 +69,7 @@ def main():
     field_names = [f.field_name for f in gps_dump_data.field_data]
     if not 'len' in field_names or not 'data[0]' in field_names:
         print('Error: gps_dump message has wrong format')
-        exit(-1)
+        sys.exit(-1)
 
     if len(ulog.dropouts) > 0:
         print("Warning: file contains {0} dropouts".format(len(ulog.dropouts)))
@@ -73,12 +79,15 @@ def main():
     with open(to_dev_filename, 'wb') as to_dev_file:
         with open(from_dev_filename, 'wb') as from_dev_file:
             msg_lens = gps_dump_data.data['len']
+            instances = gps_dump_data.data.get('instance', [0]*len(msg_lens))
             for i in range(len(gps_dump_data.data['timestamp'])):
+                instance = instances[i]
                 msg_len = msg_lens[i]
-                if msg_len & (1<<7):
-                    msg_len = msg_len & ~(1<<7)
-                    file_handle = to_dev_file
-                else:
-                    file_handle = from_dev_file
-                for k in range(msg_len):
-                    file_handle.write(gps_dump_data.data['data['+str(k)+']'][i])
+                if instance == required_instance:
+                    if msg_len & (1<<7):
+                        msg_len = msg_len & ~(1<<7)
+                        file_handle = to_dev_file
+                    else:
+                        file_handle = from_dev_file
+                    for k in range(msg_len):
+                        file_handle.write(gps_dump_data.data['data['+str(k)+']'][i])
