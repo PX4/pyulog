a159d7650a3f05e4d2e5953e738302a0c41eb932
Beat Küng <beat-kueng@gmx.net>
2026-08-06 10:20:09 +0200

    fix setup: explicitly copy symlink
    Newer versions of setuptools are more strict.
    Error that was raised:
    error: can't copy 'pyulog/libevents_parse': doesn't exist or not a regular file

--- setup.py
+++ setup.py
@@ -7,14 +7,33 @@ The file format is documented on https://docs.px4.io/main/en/dev_log/ulog_file_f
 
 """
 
+from pathlib import Path
+import shutil
+
 from setuptools import setup
+from setuptools.command.build_py import build_py
 
 DOCLINES = __doc__.split("\n")
+LIBEVENTS_PARSE_PACKAGE = "libevents_parse"
 
 # pylint: disable=invalid-name
+
+class BuildPy(build_py):
+
+    def copy_file(self, infile, outfile, *args, **kwargs):
+        infile = Path(infile)
+        resolved = infile.resolve()
+
+        if infile.name == LIBEVENTS_PARSE_PACKAGE and resolved.is_dir():
+            # Copy libevents_parse as setuptools does not allow to copy the symlink
+            shutil.copytree(resolved, outfile, dirs_exist_ok=True)
+            return str(outfile), True
+
+        return super().copy_file(infile, outfile, *args, **kwargs)
 
 setup(
     long_description="\n".join(DOCLINES),
     long_description_content_type='text/x-rst',
     platforms=["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+    cmdclass={"build_py": BuildPy},
 )
